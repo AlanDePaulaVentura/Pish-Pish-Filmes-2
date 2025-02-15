c563b247d6781b956eae8ae4262b3ae56ff06705
AlanDePaulaVentura <159501593+AlanDePaulaVentura@users.noreply.github.com>
2025-02-15 09:24:15 -0300

    Add files via upload

---
+++ js/filmes.js
@@ -0,0 +1,29 @@
+//aqui é a splash no início da Home
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.getElementById("splash").style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("splash").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }, 1000);
+    }, 3000);
+});
+//Final da Splash
+//inicio da caixa de login
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("loginModal");
+    const closeBtn = document.querySelector(".close");
+    if (modal) {
+      modal.style.display = "flex";
+      closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+        window.history.back();
+      });
+      window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+          modal.style.display = "none";
+          window.history.back();
+        }
+      });
+    }
+  });
